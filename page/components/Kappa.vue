aab726ad58434712085480671f1a13c0f45206bf
mochisuna <hirokazu.maruta@gmail.com>
2025-03-20 19:27:57 +0900

    wip animation

--- page/components/Kappa.vue
+++ page/components/Kappa.vue
@@ -1,24 +1,17 @@
 <template>
-  <div id="target">
-    <KappaSVG id = "kappa"/>
-    <div class="button" id="button1"></div>
-    <!-- <div class="button" id="button2"></div>
-    <div class="button" id="button3"></div>
-    <div class="button" id="button4"></div> -->
+  <div id="base">
+    <div id="target">
+      <KappaSVG id="kappa" />
+      <div class="button" id="button1"></div>
+      <div class="button" id="button2"></div>
+      <div class="button" id="button3"></div>
+      <div class="button" id="button4"></div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import KappaSVG from "@/assets/kappa.svg"; // SVG を直接インポート
-// import { ref, onMounted } from "vue";
-// const svgContainer = ref(null);
-
-// onMounted(() => {
-//   if (svgContainer.value) {
-//     svgContainer.value.innerHTML = svgContent;
-//   }
-// });
-
 const isActive = ref(false);
 
 const toggleAnimation = () => {
@@ -27,106 +20,106 @@ const toggleAnimation = () => {
 </script>
 
 <style>
-/* #kappa { */
-  .kappa-cloud{
-    top: calc(50% - 100px);
-    left: calc(50% - 100px);
-  }
-  .kappa-animation-group-1 {
-    transform-origin: center center;
-    animation: scale-animation-1 0.8s ease-in-out infinite alternate;
-  }
-  .kappa-animation-group-2 {
-    transform-origin: center center;
-    animation: scale-animation-2 1s ease-in-out infinite alternate;
-  }
-  .kappa-animation-group-3 {
-    transform-origin: center center;
-    animation: scale-animation-3 0.7s ease-in-out infinite alternate;
-  }
-  
-  .eye {
-    animation: blink 10s linear infinite;
-    transform-origin: center bottom;
-  }
-  
-  .kappa-frame-set,
-  .kappa-element-set {
-    transform-origin: center center;
-    animation: appear-animation 0.8s ease-in forwards;
-  }
-  .kappa-body {
-    transition-duration: 0.4s;
-    transform-origin: center center;
-    transform: scale(0, 0);
-  }
-/* } */
-
-#target {
-  /* top: calc(50% - 100px);
-  left: calc(50% - 100px);
-  position: absolute;
-  transform-origin: center center;
-  background-color: #44435d; */
+#base {
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: #FFF4CC;
+  background-color: #fff4cc;
+}
+#target {
+  position: absolute;
+  left: calc(50% - 400px);
+  top: calc(50% - 400px);
+  width: 800px;
+  height: 800px;
 }
 
-
+#kappa {
+  position: absolute;
+  left: calc(50% - 250px);
+  top: calc(50% - 250px);
+  z-index: 2;
+}
+.kappa-animation-group-1 {
+  transform-origin: center;
+  animation: scale-animation-1 0.8s ease-in-out infinite alternate;
+}
+.kappa-animation-group-2 {
+  transform-origin: center;
+  animation: scale-animation-2 1s ease-in-out infinite alternate;
+}
+.kappa-animation-group-3 {
+  transform-origin: center;
+  animation: scale-animation-3 0.7s ease-in-out infinite alternate;
+}
+.eye {
+  animation: blink 10s linear infinite;
+  transform-origin: center bottom;
+}
+.kappa-frame-set,
+.kappa-element-set {
+  transform-origin: center;
+  animation: appear-animation 0.8s ease-in forwards;
+}
+.kappa-body {
+  transition-duration: 0.4s;
+  transform-origin: center;
+  transform: scale(0, 0);
+}
 .button {
-  top: calc(50% - 100px);
-  left: calc(50% - 100px);
-  width: 50px;
-  height: 50px;
+  top: calc(50% - 60px);
+  left: calc(50% - 70px);
+  width: 120px;
+  height: 120px;
+  background-color: #91ffab;
+  border-color: #707070;
+  border-width: 7px;
+  border-style: solid;
   border-radius: 100%;
   position: absolute;
-  background-color: #91ffab;
-  border-color : #707070;
-  border-width : 7px;
-  border-style : solid;
+  transition: transform 0.5s ease;
 }
 
 #target:hover {
-  #kappa{
-    .kappa-frame-set,
-    .kappa-element-set {
-      transform-origin: center center;
-      animation: disappear-animation 0.5s ease-in forwards;
-    }
-    .kappa-body {
-      transition-delay: 0.2s;
-      transition-duration: 0.3s;
-      transform-origin: center center;
-      transform: scale(1, 1);
-    }
+  .kappa-frame-set,
+  .kappa-element-set {
+    transform-origin: center;
+    animation: disappear-animation 0.7s ease-in forwards;
   }
-  /* .button {
-    
-  } */
+  .kappa-body {
+    transition-delay: 0.2s;
+    transition-duration: 0.3s;
+    transform-origin: center;
+    transform: scale(1, 1);
+  }
+  .button {
+    border-radius: 10%;
+    transition: transform 0.5s ease;
+    transition-delay: 0.5s;
+  }
+
   #button1 {
+    transform: translate(-250px, -250px);
     /* animation: button-animation 1s ease-in forwards; */
   }
 
   #button2 {
-    /* transform: translate(-80px, -80px); */
+    transform: translate(-250px, 250px);
     /* animation: button-animation 1s ease-in forwards; */
   }
 
   #button3 {
-    /* transform: translate(80px, -80px); */
+    transform: translate(250px, -250px);
     /* animation: button-animation 1s ease-in forwards; */
   }
 
   #button4 {
-    /* transform: translate(80px, 80px); */
+    transform: translate(250px, 250px);
     /* animation: button-animation 1s ease-in forwards; */
   }
 }
-
 
 @keyframes scale-animation-1 {
   0% {
@@ -208,40 +201,29 @@ const toggleAnimation = () => {
   94%,
   100% {
     transform: scaleY(1);
-    transform-origin: center center;
+    transform-origin: center;
   }
   98% {
     transform: scaleY(0.1);
-    transform-origin: center center;
+    transform-origin: center;
   }
 }
 
 @keyframes button-animation {
   0% {
-    /* transform: translate(0px, 0px); */
     background-color: #91ffab;
-    stroke: #707070;
-    stroke-width: 3;
-    /* width: 0px;
-    height: 0px;
-    opacity: 0.5; */
-    border-radius: 0%;
+    border-color: #707070;
   }
   30% {
-    opacity: 1;
   }
   50% {
-    border-radius: 30%;
+    background-color: #91ffab;
+  }
+  90% {
+    background-color: #ff6655;
   }
   100% {
-    width: 250px;
-    height: 100px;
-    background-color: #ff6655;
-    stroke-width: 3;
-    stroke: #ffffff;
     border-radius: 10%;
   }
 }
-
-
 </style>
